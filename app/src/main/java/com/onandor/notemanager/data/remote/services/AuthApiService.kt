cfe3204438cec8d3363835ad0767783525fb58f1
Oláh Nándor <olahnandi00@gmail.com>
2023-10-14 16:32:29 +0200

    Change auth API routes to constants

--- app/src/main/java/com/onandor/notemanager/data/remote/services/AuthApiService.kt
+++ app/src/main/java/com/onandor/notemanager/data/remote/services/AuthApiService.kt
@@ -3,37 +3,50 @@ package com.onandor.notemanager.data.remote.services
 import com.onandor.notemanager.data.remote.models.AuthUser
 import com.onandor.notemanager.data.remote.models.TokenPair
 import com.onandor.notemanager.data.remote.models.UserDetails
+import com.onandor.notemanager.data.remote.services.AuthEndpoints.CHANGE_PASSWORD
+import com.onandor.notemanager.data.remote.services.AuthEndpoints.DELETE
+import com.onandor.notemanager.data.remote.services.AuthEndpoints.LOGIN
+import com.onandor.notemanager.data.remote.services.AuthEndpoints.LOGOUT
+import com.onandor.notemanager.data.remote.services.AuthEndpoints.REGISTER
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.request.post
 import io.ktor.client.request.setBody
-import io.ktor.client.statement.bodyAsText
 import javax.inject.Inject
+
+private const val AUTH_ROUTE = "auth/"
+private object AuthEndpoints {
+    const val REGISTER = "register"
+    const val LOGIN = "login"
+    const val LOGOUT = "logout"
+    const val DELETE = "delete"
+    const val CHANGE_PASSWORD = "changePassword"
+}
 
 class AuthApiService @Inject constructor(
     private val httpClient: HttpClient
 ) : IAuthApiService {
 
     override suspend fun register(authUser: AuthUser): UserDetails {
-        return httpClient.post("auth/register") {
+        return httpClient.post("$AUTH_ROUTE$REGISTER") {
             setBody(authUser)
         }.body()
     }
 
     override suspend fun login(authUser: AuthUser): TokenPair {
-        return httpClient.post("auth/login") {
+        return httpClient.post("$AUTH_ROUTE$LOGIN") {
             setBody(authUser)
         }.body()
     }
 
     override suspend fun logout(authUser: AuthUser) {
-        httpClient.post("auth/logout") {
+        httpClient.post("$AUTH_ROUTE$LOGOUT") {
             setBody(authUser)
         }
     }
 
     override suspend fun deleteUser(password: String) {
-        httpClient.post("auth/delete") {
+        httpClient.post("$AUTH_ROUTE$DELETE") {
             setBody(password)
         }
     }
@@ -43,7 +56,7 @@ class AuthApiService @Inject constructor(
         oldPassword: String,
         newPassword: String
     ): HashMap<String, String> {
-        return httpClient.post("auth/changePassword") {
+        return httpClient.post("$AUTH_ROUTE$CHANGE_PASSWORD") {
             setBody(
                 hashMapOf(
                     "deviceId" to installationId,
