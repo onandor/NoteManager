7e648ad8e9060a5002ac5cbeeb6ca22cf6203566
Oláh Nándor <olahnandi00@gmail.com>
2023-10-16 20:48:04 +0200

    Upgrade dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,19 +53,19 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
 
     // Compose
-    implementation("androidx.activity:activity-compose:1.7.2")
+    implementation("androidx.activity:activity-compose:1.8.0")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3:1.2.0-alpha09")
     implementation("androidx.compose.material:material:1.5.3")
-    implementation("androidx.navigation:navigation-compose:2.6.0")
-    implementation("androidx.compose.runtime:runtime-livedata:1.5.1")
+    implementation("androidx.navigation:navigation-compose:2.7.4")
+    implementation("androidx.compose.runtime:runtime-livedata:1.5.3")
     implementation("androidx.compose.foundation:foundation:1.6.0-alpha07")
 
     // Serialization
