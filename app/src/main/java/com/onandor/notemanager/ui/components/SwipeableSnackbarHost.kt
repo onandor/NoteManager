6a171b27d351c024f82b5b1e660a04fa65d59b41
Oláh Nándor <olahnandi00@gmail.com>
2023-12-05 17:31:06 +0100

    Fix dismissing first snackbar dismisses the next one as well

--- app/src/main/java/com/onandor/notemanager/ui/components/SwipeableSnackbarHost.kt
+++ app/src/main/java/com/onandor/notemanager/ui/components/SwipeableSnackbarHost.kt
@@ -5,39 +5,33 @@ import androidx.compose.material3.DismissState
 import androidx.compose.material3.DismissValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.SnackbarHostState
-import androidx.compose.material3.SwipeToDismiss
+import androidx.compose.material3.SwipeToDismissBox
 import androidx.compose.material3.rememberDismissState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
+import kotlinx.coroutines.delay
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SwipeableSnackbarHost(
     hostState: SnackbarHostState,
     modifier: Modifier = Modifier,
-    dismissSnackbarState: DismissState = rememberDismissState(
-        confirmValueChange = { value ->
-            if (value != DismissValue.Default) {
-                hostState.currentSnackbarData?.dismiss()
-                true
-            }
-            else {
-                false
-            }
-        }
-    ),
-    dismissContent: @Composable RowScope.() -> Unit
+    state: DismissState = rememberDismissState(),
+    content: @Composable RowScope.() -> Unit
 ) {
-    LaunchedEffect(dismissSnackbarState.currentValue) {
-        if (dismissSnackbarState.currentValue != DismissValue.Default) {
-            dismissSnackbarState.reset()
+    LaunchedEffect(state.currentValue) {
+        if (state.currentValue != DismissValue.Default) {
+            hostState.currentSnackbarData?.dismiss()
+            delay(100)
+            state.snapTo(DismissValue.Default)
         }
     }
-    SwipeToDismiss(
+
+    SwipeToDismissBox(
         modifier = modifier,
-        state = dismissSnackbarState,
-        background = {},
-        dismissContent = dismissContent,
+        state = state,
+        backgroundContent = {},
+        content = content
     )
 }
